a6b7c8bb81ca2e6f3ffd02d0c6957a28eee87cdb
wangyiex <wyd18744021607@gmail.com>
2016-02-02 19:17:03 -0500

    change the server.js content to make it simple make another app get section

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req,res){
+app.use(express.static(__dirname + '/public'));
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/hello', function(req, res){
     res.send('hello world');
 });
-app.get('/a', function(req,res){
-    res.send('hello worldaaaa');
-});
-
-app.listen(3000);
+app.listen(port, ipaddress);
