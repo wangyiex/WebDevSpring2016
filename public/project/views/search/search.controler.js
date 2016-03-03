d1f2df6e22970ad3c914b878af21b7187661c99e
wangyiex <wyd18744021607@gmail.com>
2016-03-03 16:40:02 -0500

    start work the new project

--- public/project/views/search/search.controler.js
+++ public/project/views/search/search.controler.js
@@ -4,15 +4,12 @@
         .controller("SearchController",SearchController);
     function SearchController($scope, $http) {
         $scope.search = search;
-        $scope.title="star war";
-
+        $scope.title = "star war";
         function search(title) {
-            console.log(title);
-            $http.get("http://api.glassdoor.com/api/api.htm?t.p=54774&t.k=JjQc4RmthO&userip=0.0.0.0&" +
-                "useragent=&format=json&v=1&action=employers&q="+title)
+            $http.jsonp("http://api.glassdoor.com/api/api.htm?t.p=54774&t.k=JjQc4RmthO&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&callback=JSON_CALLBACK&q="+title)
                 .success(function(response) {
                     console.log(response);
-                    $scope.data = response;
+                    $scope.data = response.response.employers;
                 })
         }
     }
