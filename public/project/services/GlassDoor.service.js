43b0a6a9a0a58641346c6578394ea6e7d0daed28
wangyiex <wyd18744021607@gmail.com>
2016-03-09 13:08:02 -0500

    We can make a comment now version2

--- public/project/services/GlassDoor.service.js
+++ public/project/services/GlassDoor.service.js
@@ -3,7 +3,7 @@
         .module("JobMarketApp")
         .factory("GlassDoorService",GlassDoorService);
 
-    function GlassDoorService($http) {
+    function GlassDoorService($http,$location) {
         var company = [
             {	"id":123, "name":"EMC","reviews":[{"name":"alice","review":"this is a great company"},
                 {"name":"bob","review":"this company is the perfect place to work for"}]}
@@ -32,11 +32,25 @@
         }
 
         function leftreview(name,user_review,currentuser) {
-            var newreivew={"name":currentuser,"review":user_review};
+            var newreview;
+            var employer;
+            if(currentuser) {
+                newreview = {"name":currentuser,"review":user_review};
+            }else {
+                newreview = {"name":"anonymous","review":user_review};
+            }
             for(r in company) {
                 if(company[r].name==name) {
-                   company[r].reviews.push(newreivew);
+                   employer = company[r];
                 }
+            }
+            if(employer) {
+                employer.reviews.push(newreview);
+            } else {
+                employer = {"id":(new Date).getTime(),"name":name,"reviews":[]};
+                employer.reviews.push(newreview);
+                company.push(employer);
+                $location.url('#/detail/'+name);
             }
         }
     }
