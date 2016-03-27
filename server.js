00254bba20e37c74d767295ab45929e7ca434cb6
wangyiex <wyd18744021607@gmail.com>
2016-03-27 17:10:53 -0400

    install cookie-parser for later usage

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 
